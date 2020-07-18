05d67421feb7c51647e550e794750385a2c8f715
Erik Zhao <erikzhao98@gmail.com>
2020-07-18 18:45:49 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -7,3 +7,13 @@ def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
+
+def link
+  link_phrase = "It's Dangerous To Go Alone! Take This."
+end
+
+def all_phrases
+  phrase
+  status
+  link_phrase
+end
